41587a3ca69ee2dd15ee950347089acee43a7162
TonnyyMenzo <anthonymatthew.nemenzo@bisu.edu.ph>
2025-04-27 21:15:59 +0800

    tonton2nd_commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,7 @@
 import { useState, useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Home.css';
-import DroneFeed from '../components/DroneFeed';
 
-function Home() {
 function Home() {
   const navigate = useNavigate();
   const [totalCorn, setTotalCorn] = useState(0);
